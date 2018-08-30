69597c543d9fc9ca3028dfaa419d45bae13727ce
Michael Vinicius de Souza Goncalves <michael.goncalves@ativas.com.br>
2018-08-30 16:42:08 -0300

    Criado uma em partes de List, Stop, Start

--- adminPortal.sh
+++ adminPortal.sh
@@ -9,8 +9,9 @@
 XMLDATA="./AdminPortalXML"
 PortalMode="$(echo "cat /config/portalvariables/portalmode/text()" | xmllint --nocdata --shell $XMLDATA |sed '1d;$d')"
 PortalHome="$(echo "cat /config/portalvariables/portalhome/text()" | xmllint --nocdata --shell $XMLDATA |sed '1d;$d')"
+PortalProfile="$(echo "cat /config/portalvariables/portalprofile/text()" | xmllint --nocdata --shell $XMLDATA |sed '1d;$d')"
 PortalLogs="$(echo "cat /config/portalvariables/portallogs/text()" | xmllint --nocdata --shell $XMLDATA |sed '1d;$d')"
-
+cachefile="./.cacheAdminPortal"
 
 #Load user Informations off xml file. 
 
@@ -23,15 +24,49 @@ AppServer="$(echo "cat /config/portalvariables/portalappserver/text()" | xmllint
 
 #Operator Functions for Portal Websphere. 
 stop(){
+
+    ARG1=$1
     echo "O Sistema esta realizando o stop do portal"
-    echo "O Sistema esta realizando o stop do portal novamnte"
- 
-    
+    if  [ $PortalMode == "standalone" ]
+        then
+            if [[ $ARG == $AppServer]]
+                then
+                    $PortalHome/AppServer/bin/stopServer.sh $ARG -username $User -password $Password
+            fi
+
+ #   if  [[ $PortalMode == "cluster" ]]
+ #       then
+
+#    fi       
+
+
+
+
+
+    fi
 }
 
 start(){
     echo "O Sistema esta realizando o Start do portal"
   
+}
+
+list(){
+    #List all AppServer and DMGR in Websphere portal
+    echo "the following servers are configured in this websphere portal"
+    if [[-s $cachefile ]]
+        then
+        echo $cachefile
+      
+    else
+        $PortalHome/AppServer/bin/serverStatus.sh -all -username $User -password $Password  |grep "Server name" |awk '{print $4}' > $cachefile 
+        echo $cachefile
+
+
+    fi
+
+
+
 }
 
 
